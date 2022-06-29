5881227a593016f9434771a2a79f484176117d3c
alegarn <alexgarnier78310@protonmail.com>
2022-06-29 14:55:37 +0200

    phase: sorting data, new select.rb: adding data

--- select.rb
+++ select.rb
@@ -1,22 +1,40 @@
 require 'rubygems'
 require "csv"
 
+rank = []
+word = []
 
-word_dict_freq = []
-word_songs = []
 n = 0
 
 CSV.open("words.csv", "r") do |row|
   row = row.to_a
-  word_dict_freq << row
+  while n < 100
+    rank << row[n][0]
+    word << row[n][1]
+    n = n + 1
+  end
 end
+
+word_dict_freq = [rank, word]
+
+id = []
+total_found = []
+word = []
+
 
 n = 0
+
 CSV.open("lyrics_per_voc_score.csv", "r") do |row|
-    row = row.to_a
-
-    word_songs << row
-
+  row = row.to_a
+  while n < 54
+    id << row[n][0]
+    word << row[n][1]
+    total_found << row[n][2]
+    n = n + 1
+  end
 end
 
+word_songs = [id, word, total_found]
+
 print word_dict_freq
+print word_songs
