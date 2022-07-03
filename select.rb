0c2cb060fa8c96e468e8203d80478b14ae59191f
alegarn <alexgarnier78310@protonmail.com>
2022-07-03 05:58:54 +0200

    is able to compare at least 2 songs

--- select.rb
+++ select.rb
@@ -24,52 +24,62 @@ end
 
 
 def lyrics
-  print "what is the song name?"
-  puts ">"
-  song_name = gets.chomp
-  # pour plusieurs, une boucle de musiques
-  id = []
-  total_found = []
-  word = []
-
-  n = 0
-
-  CSV.open("#{song_name}.txt_lyrics_per_voc_score.csv", "r") do |row|
-    row = row.to_a
-    while n < 54
-      id << row[n][0]
-      word << row[n][1]
-      total_found << row[n][2]
-      n = n + 1
+  word_songs = []
+  l = 0
+  while l == 0
+    print "what is the song name? (to quit: 0)"
+    puts ">"
+    song_name = gets.chomp
+    if song_name == "0"
+      break
     end
+    # pour plusieurs, une boucle de plusieurs musiques
+    id = []
+    total_found = []
+    word = []
+
+    n = 0
+
+    CSV.open("#{song_name}.txt_lyrics_per_voc_score.csv", "r") do |row|
+      row = row.to_a
+      while n < 54
+        id << row[n][0]
+        word << row[n][1]
+        total_found << row[n][2]
+        n = n + 1
+      end
+    end
+    word_songs << [id,song_name, word, total_found]
   end
 
-  return word_songs = [id, word, total_found]
+  return word_songs
 end
 
 def compare(word_songs, word_dict_freq)
   p = 0
   i = 1
-  words = []
   # comparer les mots à apprendre (soit les 10 premiers en anglais), se trouvant dans les paroles
 
-
-  word_dict_freq[1].map do |row|
+  word_songs.map do |lyrics|
+    words = []
+    word_dict_freq[1].map do |row|
       i = 1
       while i < 9
-        if word_songs[1][i] == row
-          words << [word_songs[1][i], word_songs[2][i]]
+        if lyrics[2][i] == row
+          words << [lyrics[2][i],lyrics[3][i]]
         end
         i = i + 1
       end
-  end
+    end
 
-  score = 0
-  words.each do |number|
-   score = score + number[1].to_i
-  end
+    score = 0
+    words.each do |number|
+      score = score + number[1].to_i
+    end
 
-  print "#{words}: score de #{score} (english top 100) "
+    print "#{lyrics[1]}#{words}: score de #{score} (english top 100) "
+    p = p + 1
+  end
 end
 
 
@@ -80,9 +90,6 @@ def perform
   file_name_selected = gets.chomp
 
   compare(lyrics,selected_voc(file_name_selected))
-
-
-
 
 end
 
