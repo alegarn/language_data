8ee83b61d65ff5b36c14d02bf43724b701324a5e
alegarn <alexgarnier78310@protonmail.com>
2022-08-24 04:29:15 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,43 +7,123 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require "faker"
+require "csv"
 
 TrackScoreFreqDictionary.destroy_all
 TrackScore.destroy_all
 Track.destroy_all
 FreqDictionary.destroy_all
 
+puts Dir.getwd
+Dir.chdir("lib/assets")
 
 
-10.times do
-  track = Track.create(
-    title: Faker::Lorem.word,
-    song_type: Faker::Music.genre ,
-    band: Faker::Music.band,
-    album: Faker::Music.album
-  )
+artists = Dir.children(Dir.pwd)
+
+artists.each do |artist|
+  if artist == "words"
+    Dir.chdir("#{artist}")
+    Dir.glob("*.csv").each do |file|
+
+      puts "#{file} un csv"
+
+      parse_csv = CSV.parse(File.read("#{file}"), headers:true)
+
+      parse_csv.each_with_index { |line, index|
+        if index == parse_csv.size - 1
+          break
+        end
+
+        freq_dictionary = FreqDictionary.create(
+          freq_rank: index + 1 ,
+          word: line[1],
+          language: "english"
+        )
+      }
+    end
+
+    puts Dir.getwd
+    Dir.chdir("../")
+    puts Dir.getwd
+  end
 end
 
-10.times do
-  i = 1
-  freq_dictionary = FreqDictionary.create(
-    freq_rank: i,
-    word: Faker::String.random(length: 2..10),
-    language: "english"
-  )
-  i = i + 1
-end
+artists.each do |artist|
 
-30.times do
-  track_score = TrackScore.create(
-    track_id: Track.all.sample.id,
-    track_word_occurence: rand(7..15)
-  )
-end
+  if File.directory?("#{artist}") && artist != "words"
+    puts "#{artist} artists"
+    Dir.chdir("#{artist}")
+    albums = Dir.children(Dir.pwd)
 
-100.times do
-  track_score_freq_dictionary = TrackScoreFreqDictionary.create(
-    track_score_id: TrackScore.all.sample.id ,
-    freq_dictionary_id: FreqDictionary.all.sample.id
-  )
+    albums.each { |album|
+      puts "#{album} albums"
+      Dir.chdir("#{album}")
+      songs = Dir.children(Dir.pwd)
+
+      songs.each do |song|
+        puts "#{song} song"
+        Dir.chdir("#{song}")
+        track = Track.create(
+          title: song,
+          song_type: "0" ,
+          band: artist,
+          album: album
+        )
+        Dir.glob("*.csv").each do |file|
+
+          puts "#{file} un csv"
+
+          w = []
+          o = []
+          a = 0
+
+          parse_csv = CSV.parse(File.read("#{file}"), headers:true)
+          parse_csv.each_with_index { |line, index|
+            if index == parse_csv.size - 1
+              break
+            end
+            a = a + 1
+            w << line[1]
+            o << line[2]
+
+          }
+          0.upto((a-1)) do |word|
+
+            begin
+              search = FreqDictionary.find_by_word("#{w[word]}")
+
+              unless search.nil?
+                track_score = TrackScore.create(
+                  track_id: Track.last.id,
+                  track_word_occurence: o[word]
+                )
+
+                track_score_freq_dictionary = TrackScoreFreqDictionary.create(
+                  track_score_id: TrackScore.last.id ,
+                  freq_dictionary_id: search.id
+                )
+
+              end
+
+            rescue => e
+              puts e.message
+
+            end
+
+          end
+
+        end
+        Dir.chdir("../")
+        puts Dir.getwd
+
+      end
+      Dir.chdir("../")
+      puts Dir.getwd
+
+    }
+    Dir.chdir("../")
+    puts Dir.getwd
+
+  end
+
 end
