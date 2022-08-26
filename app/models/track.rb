c6f65825bf9ff299770628ab1d613a8fef6dd67f
alegarn <alexgarnier78310@protonmail.com>
2022-08-26 22:01:36 +0200

    the validtions

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,8 @@
 class Track < ApplicationRecord
   has_many :track_scores
+  
+  validates :title, presence: true
+  validates :song_type, presence: true
+  validates :band, presence: true
+  validates :album, presence: true
 end
--- app/models/track_score_freq_dictionary.rb
+++ app/models/track_score_freq_dictionary.rb
@@ -1,4 +1,7 @@
 class TrackScoreFreqDictionary < ApplicationRecord
   belongs_to :track_score
   belongs_to :freq_dictionary
+
+  validates :track_score_id, presence: true
+  validates :freq_dictionary_id, presence: true
 end
