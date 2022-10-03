20f111425da3c6957d1a6488163d5fab7d211eaf
alegarn <alexgarnier78310@protonmail.com>
2022-10-04 01:59:19 +0200

    genius link

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,5 +1,16 @@
 class TracksController < ApplicationController
     def show
         @track = Track.find(params[:id])
-      end
+        @track_modified = rename(@track)
+    end
+
+    private 
+
+    def rename(track)
+      song = track.title.gsub("_", "%20").downcase()
+      album = track.album.gsub("_", "%20").downcase()
+      band = track.band.gsub("_", "%20").downcase()
+
+      return {title: song, album: album, band: band}
+    end
 end
