5606dd2b1538a09540e01b9d212956c63372571e
alegarn <alexgarnier78310@protonmail.com>
2022-10-10 16:14:04 +0200

    refacto string to link method

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,16 +1,21 @@
 class TracksController < ApplicationController
-    def show
-        @track = Track.find(params[:id])
-        @track_modified = rename(@track)
-    end
+  def show
+      @track = Track.find(params[:id])
+      @track_modified = rename(@track)
+  end
 
-    private 
+  private 
 
-    def rename(track)
-      song = track.title.gsub("_", "%20").downcase()
-      album = track.album.gsub("_", "%20").downcase()
-      band = track.band.gsub("_", "%20").downcase()
+  def rename(track)
+    song = format_link(track.title)
+    album = format_link(track.album)
+    band = format_link(track.band)
 
-      return {title: song, album: album, band: band}
-    end
+    return {title: song, album: album, band: band}
+  end
+
+  def format_link(string)
+    string.gsub("_", "%20").downcase()
+  end
+
 end
